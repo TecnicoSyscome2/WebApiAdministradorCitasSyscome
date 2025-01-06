5a7dfc597df933860355ef68a5b2e350aece169e
TecnicoSyscome2 <tecnico2syscome@gmail.com>
2025-01-06 08:59:18 -0600

    FirstCommit
    Creacion de Proyecto Inicial 06/01/2024 Tecnico 2

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using WebApi.Citas.Administrador.DAL;
+using WebApi.Citas.Administrador.Modelos;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Configurar DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<BDConexion>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
+
